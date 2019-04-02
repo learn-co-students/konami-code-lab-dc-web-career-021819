afe32f75ecb4858159fbd0fe9032842b066e8e9a
mmorales4050 <mmorales4050@gmail.com>
2019-04-02 17:55:33 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,6 @@
-const codes = [
+document.addEventListener('keydown', onKeyDownHandler)
+
+const alphabet = [
   "ArrowUp",
   "ArrowUp",
   "ArrowDown",
@@ -11,6 +13,26 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+// Keys for A, B, and C keys.
+ //= ['a', 'b', 'c'];
+
+// Keep track of index outside of the event handler.
+let index = 0;
+
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  console.log(e)
+  const key = e.key;
+
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
