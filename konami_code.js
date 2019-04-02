cd228d1e9eb2ad21f5bb690e1c9e78dcd2f12d72
github username <github email address>
2019-04-02 14:10:43 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,25 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const body = document.body
+
+  let codesCorrect = 0;
+
+  function onKeyDownHandler(event){
+    const key = event.key
+    if (key === codes[codesCorrect]){
+      if(codesCorrect === (codes.length - 1)){
+        alert("Congrats!");
+        codesCorrect = 0;
+      }
+      else{
+        codesCorrect++;
+      }
+    }
+    else {
+      codesCorrect = 0;
+    }
+  }
+  body.addEventListener('keydown', onKeyDownHandler, false);
+
 }
