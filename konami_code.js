62ab349fe4747062338198b9c0a827f4fafad133
Sean <shonkelli@gmail.com>
2019-04-03 17:06:58 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,30 @@ const codes = [
 
 function init() {
   // your code here
+
+  // console.log("what is this?")
 }
+
+let index = 0
+
+const input = document.querySelector('input');
+const body = document.querySelector('input');
+function cheatCode(e){
+  const key = e.key
+  console.log(key, index)
+  if (key === codes[index]) {
+    index++
+    if (index === codes.length){
+      alert("flawless victory");
+      index = 0
+    }
+
+  } else {
+    index = 0
+  }
+
+}
+
+input.addEventListener('keydown', cheatCode)
+
+body.addEventListener('keydown', cheatCode)
