81ca8296845a964e63f15486c0bfa1fe304905c7
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-02 22:55:58 -0400

    add solution

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
-}
+  let index = 0;
+  function onKeyDownHandler(e){
+    const key = e.key;
+    if (key === codes[index]){
+      index++;
+      if (index === codes.length){
+        alert("Konami Code!!");
+        index = 0;
+      }
+    }else{
+      index = 0;
+    }
+  }
+  document.body.addEventListener('keydown', function(e){onKeyDownHandler(e)});
+};
